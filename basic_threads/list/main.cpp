4e83db88723e1940dac252adc769fb300e17c578
diplay <diplaySuper@gmail.com>
2023-02-24 19:33:17 +0400

    stronger test

--- basic_threads/list/main.cpp
+++ basic_threads/list/main.cpp
@@ -17,6 +17,13 @@ struct ListItem {
 };
 
 constexpr size_t elementsCount = 1000000;
+enum class Event {
+    Erase = 0,
+    Iterate
+};
+
+std::list<Event> events;
+std::mutex eventsMutex;
 
 void ThreadFunction(ThreadSafeList<ListItem>& list) {
     const auto thread_id = std::this_thread::get_id();
@@ -38,18 +45,42 @@ void ThreadFunction(ThreadSafeList<ListItem>& list) {
             if (item.reachOrder.size() == 1) {
                 {
                     std::lock_guard<std::mutex> guard(coutMutex);
+                    std::lock_guard<std::mutex> guardEvents(eventsMutex);
                     std::cout << "Thread " << thread_id << " first reached sum " << sum << std::endl;
+                    events.push_back(Event::Iterate);
                 }
                 std::this_thread::sleep_for(20ms * counter / partSize);
             } else {
                 std::lock_guard<std::mutex> guard(coutMutex);
+                std::lock_guard<std::mutex> guardEvents(eventsMutex);
                 std::cout << "Thread " << thread_id << " reached sum " << sum << std::endl;
+                events.push_back(Event::Iterate);
             }
         }
     }
     {
         std::lock_guard<std::mutex> guard(coutMutex);
         std::cout << "Thread " << thread_id << " finished with sum " << sum << std::endl;
+    }
+}
+
+void EraseThreadFunction(ThreadSafeList<ListItem>& list) {
+    const auto thread_id = std::this_thread::get_id();
+    size_t counter = 0;
+    const size_t partSize = (elementsCount / 10);
+    for (ThreadSafeList<ListItem>::Iterator it = --list.end(); it != list.begin();) {
+        ++counter;
+        if (counter % partSize == 0) {
+            auto iterToErase = it;
+            --it;
+            list.erase(iterToErase);
+            std::lock_guard<std::mutex> guard(coutMutex);
+            std::lock_guard<std::mutex> guardEvents(eventsMutex);
+            std::cout << "Thread " << thread_id << " erased something" << std::endl;
+            events.push_back(Event::Erase);
+        } else {
+            --it;
+        }
     }
 }
 
@@ -77,7 +108,7 @@ int main() {
             });
         }
 
-        for (auto &thread: threads) {
+        for (auto &thread : threads) {
             thread.join();
         }
 
@@ -86,6 +117,13 @@ int main() {
         multithreadDuration = std::chrono::duration_cast<std::chrono::seconds>(duration);
 
         std::cout << threads.size() << " threads have run in " << multithreadDuration.count() << " seconds" << std::endl;
+
+        size_t realElementsCount = 0;
+        for (const auto& item : list) {
+            std::ignore = item;
+            ++realElementsCount;
+        }
+        assert(realElementsCount == elementsCount);
     }
 
     {
@@ -97,8 +135,9 @@ int main() {
         for (size_t i = 0; i < threadsCount; ++i) {
             threads.emplace_back(ThreadFunction, std::ref(list));
         }
+        threads.emplace_back(EraseThreadFunction, std::ref(list));
 
-        for (auto &thread: threads) {
+        for (auto &thread : threads) {
             thread.join();
         }
 
@@ -107,6 +146,32 @@ int main() {
         multithreadDuration = std::chrono::duration_cast<std::chrono::seconds>(duration);
 
         std::cout << threads.size() << " threads have run in " << multithreadDuration.count() << " seconds" << std::endl;
+
+        size_t realElementsCount = 0;
+        for (const auto& item : list) {
+            std::ignore = item;
+            ++realElementsCount;
+        }
+        assert(realElementsCount == elementsCount - 9);
+
+        bool foundEraseBetweenIterations = false;
+        for (auto it = events.begin(); !foundEraseBetweenIterations && it != events.end(); ++it) {
+            auto next = std::next(it);
+            if (next == events.end()) {
+                break;
+            }
+
+            auto nextNext = std::next(next);
+            if (nextNext == events.end()) {
+                break;
+            }
+
+            if (*it == Event::Iterate && *next == Event::Erase && *nextNext == Event::Iterate) {
+                foundEraseBetweenIterations = true;
+            }
+        }
+
+        assert(foundEraseBetweenIterations);
     }
 
     std::list<std::thread::id> prevReachOrder;
