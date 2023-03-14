1cfb6aa16999bc696a187d06d6a9fc75b0cc891e
diplay <diplaySuper@gmail.com>
2023-03-14 18:19:03 +0400

    hadred checks

--- atomic/shared_mutex/check.sh
+++ atomic/shared_mutex/check.sh
@@ -2,6 +2,9 @@
 
 defines=$(grep -c '#define' task.h)
 undefs=$(grep -c '#undef' task.h)
-includes=$(grep -E -c '#include ["<](shared_)?mutex[">]' task.h)
+includes=$(grep -E -c '#include *["<](shared_)?mutex[">]' task.h)
+using_namespace_std=$(grep -E -c 'using *namespace *std' task.h)
+std_mutex=$(grep -c 'std::mutex' task.h)
+std_shared_mutex=$(grep -c 'std::shared_mutex' task.h)
 
-[[ $undefs = 0 ]] && [[ $defines = 0 ]] && [[ $includes = 0 ]]
+[[ $undefs = 0 ]] && [[ $defines = 0 ]] && [[ $includes = 0 ]] && [[ $using_namespace_std = 0 ]] && [[ $std_shared_mutex = 0 ]] && [[ $std_mutex = 0 ]]
