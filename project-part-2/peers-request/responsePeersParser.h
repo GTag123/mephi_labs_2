5319f07e8f03fe2e4150632331e48f0993e85013
Tagir <gtagir11@yandex.ru>
2023-04-15 15:12:12 +0300

    test connections

--- project-part-2/peers-request/responsePeersParser.h
+++ project-part-2/peers-request/responsePeersParser.h
@@ -7,12 +7,12 @@ std::vector<Peer> parsePeers(const std::string& peersHash){
     for (int i = 0; i < peersHash.size(); i += 6) {
         std::string ip_string;
         for (int j = 0; j < 4; j++){
-            ip_string += std::to_string(static_cast<unsigned char>((int) peersHash[i+j]));
+            ip_string += std::to_string(static_cast<unsigned char>(peersHash[i+j]));
             if (j < 3) {
                 ip_string += ".";
             }
         }
-        auto port = static_cast<uint16_t>(((int) peersHash[i+4]) << 8) |  static_cast<uint16_t>((int) peersHash[i+5]);
+        auto port = (static_cast<uint16_t>((peersHash[i+4])) << 8) + static_cast<uint16_t>(peersHash[i+5]);
         std::unordered_map<std::string, std::string> peerMap;
         parsedPeers.push_back({ip_string, port});
     }
