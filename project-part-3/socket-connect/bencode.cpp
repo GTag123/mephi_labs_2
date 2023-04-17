598f2cd512f501d45583cc21ba8c8145e0780c9f
Tagir <gtagir11@yandex.ru>
2023-04-17 18:18:27 +0300

    test

--- project-part-3/socket-connect/bencode.cpp
+++ project-part-3/socket-connect/bencode.cpp
@@ -88,12 +88,13 @@ namespace Bencode {
         for (int i = 0; i < peersHash.size(); i += 6) {
             std::string ip_string;
             for (int j = 0; j < 4; j++){
-                ip_string += std::to_string(static_cast<unsigned char>(peersHash[i+j]));
+                ip_string += std::to_string(uint8_t(static_cast<unsigned char>(peersHash[i+j])));
                 if (j < 3) {
                     ip_string += ".";
                 }
             }
-            auto port = static_cast<uint16_t>(((int) peersHash[i+4]) << 8) |  static_cast<uint16_t>((int) peersHash[i+5]);
+            auto port = (uint16_t(static_cast<unsigned char>(peersHash[i + 4])) << 8)
+                        + uint16_t(static_cast<unsigned char>(peersHash[i + 5]));
             std::unordered_map<std::string, std::string> peerMap;
             parsedPeers.push_back({ip_string, port});
         }
