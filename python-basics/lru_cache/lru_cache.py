bd1178c0f27aa62748df68033bf23cbce2f4998f
Tagir <gtagir11@yandex.ru>
2023-05-01 17:39:18 +0300

    123

--- python-basics/lru_cache/lru_cache.py
+++ python-basics/lru_cache/lru_cache.py
@@ -3,17 +3,25 @@ from collections import OrderedDict, namedtuple
 from functools import wraps
 
 def lru_cache(max_items: int) -> Callable:
-    """
-    Функция создает декоратор, позволяющий кэшировать результаты выполнения обернутой функции по принципу LRU-кэша.
-    Размер LRU кэша ограничен количеством max_items. При попытке сохранить новый результат в кэш, в том случае, когда
-    размер кэша уже равен max_size, происходит удаление одного из старых элементов, сохраненных в кэше.
-    Удаляется тот элемент, к которому обращались давнее всего.
-    Также у обернутой функции должен появиться атрибут stats, в котором лежит объект с атрибутами cache_hits и
-    cache_misses, подсчитывающие количество успешных и неуспешных использований кэша.
-    :param max_items: максимальный размер кэша.
-    :return: декоратор, добавляющий LRU-кэширование для обернутой функции.
-    """
+    cache = OrderedDict()
+    stats = {'cache_hits': 0, 'cache_misses': 0}
     def decorator(f):
-        return f
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            key = (args, tuple(sorted(kwargs.items())))
+            if key in cache:
+                stats['cache_hits'] += 1
+                cache.move_to_end(key)
+            else:
+                stats['cache_misses'] += 1
+                result = f(*args, **kwargs)
+                cache[key] = result
+                if len(cache) > max_items:
+                    cache.popitem(last=False)
+                return result
+
+        wrapper.cache = cache
+        wrapper.stats = stats
+        return wrapper
 
     return decorator
