8d76e70afbc60107b77c0dfa5459791890dc052d
Tagir <gtagir11@yandex.ru>
2023-05-01 15:26:22 +0300

    123

--- python-basics/enhanced_list/enhanced_list.py
+++ python-basics/enhanced_list/enhanced_list.py
@@ -9,4 +9,34 @@ class EnhancedList(list):
         - если новая длина больше старой, то дополнить список значениями None;
         - если новая длина меньше старой, то удалять значения из конца списка.
     """
-    pass
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @property
+    def first(self):
+        return self[0]
+
+    @first.setter
+    def first(self, value):
+        self[0] = value
+
+    @property
+    def last(self):
+        return self[-1]
+
+    @last.setter
+    def last(self, value):
+        self[-1] = value
+
+    @property
+    def size(self):
+        return len(self)
+
+    @size.setter
+    def size(self, new_size):
+        current_size = len(self)
+        if new_size > current_size:
+            self.extend([None] * (new_size - current_size))
+        elif new_size < current_size:
+            del self[new_size:]
