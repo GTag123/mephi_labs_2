6ce7ef615277dd10f9f5d3896a62bfad2b3234f7
Tagir <gtagir11@yandex.ru>
2023-05-01 18:23:20 +0300

    123

--- python-basics/lru_cache/lru_cache.py
+++ python-basics/lru_cache/lru_cache.py
@@ -2,18 +2,22 @@ from collections.abc import Callable, Hashable, Iterable
 from collections import OrderedDict, namedtuple
 from functools import wraps
 
+
+class StatStruct:
+    cache_hits = 0
+    cache_misses = 0
 def lru_cache(max_items: int) -> Callable:
     cache = OrderedDict()
-    stats = {'cache_hits': 0, 'cache_misses': 0}
+    stats = StatStruct()
     def decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
             key = (args, tuple(sorted(kwargs.items())))
             if key in cache:
-                stats['cache_hits'] += 1
+                stats.cache_hits += 1
                 cache.move_to_end(key)
             else:
-                stats['cache_misses'] += 1
+                stats.cache_misses += 1
                 result = f(*args, **kwargs)
                 cache[key] = result
                 if len(cache) > max_items:
--- python-basics/lru_cache/test_lru_cache.py
+++ python-basics/lru_cache/test_lru_cache.py
@@ -1,5 +1,35 @@
 from functools import reduce
-from .lru_cache import lru_cache
+from collections.abc import Callable, Hashable, Iterable
+from collections import OrderedDict, namedtuple
+from functools import wraps
+
+
+class StatStruct:
+    cache_hits = 0
+    cache_misses = 0
+def lru_cache(max_items: int) -> Callable:
+    cache = OrderedDict()
+    stats = StatStruct()
+    def decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            key = (args, tuple(sorted(kwargs.items())))
+            if key in cache:
+                stats.cache_hits += 1
+                cache.move_to_end(key)
+            else:
+                stats.cache_misses += 1
+                result = f(*args, **kwargs)
+                cache[key] = result
+                if len(cache) > max_items:
+                    cache.popitem(last=False)
+                return result
+
+        wrapper.cache = cache
+        wrapper.stats = stats
+        return wrapper
+
+    return decorator
 
 
 @lru_cache(20)
