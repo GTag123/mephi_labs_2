2f7efccaaba56fb90909e8cbcc1614fd9c6d97cb
Tagir <gtagir11@yandex.ru>
2023-05-01 15:15:15 +0300

    123

--- python-basics/flatten/flatten.py
+++ python-basics/flatten/flatten.py
@@ -2,9 +2,8 @@ from collections.abc import Iterable, Generator
 
 
 def flatten(iterable: Iterable) -> Generator:
-    """
-    Генератор flatten принимает итерируемый объект iterable и с помощью обхода в глубину отдает все вложенные объекты.
-    Для любых итерируемых вложенных объектов, не являющихся строками, нужно делать рекурсивный заход.
-    В результате генератор должен пробегать по всем вложенным объектам на любом уровне вложенности.
-    """
-    pass
+    for item in iterable:
+        if isinstance(item, Iterable) and not isinstance(item, (str, bytes)):
+            yield from flatten(item)
+        else:
+            yield item
